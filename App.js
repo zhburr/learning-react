2bf421e04c6e405492f34093ddf303dc934bffab
Zohaib ur Rehman <mianzhb@gmail.com>
2025-10-18 15:47:23 +0500

    initiated the app using parcel

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
-const heading = React.createElement("h1", {id:'heading'}, "Hello world from react");
-const heading2 = React.createElement("h1", {id:'heading'}, "Hello world from react2");
-const parent = React.createElement("div",{id  : 'parent'}, React.createElement('div',{id : 'child'},[heading,heading2]))
+ import React from 'react'
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading",key : 1 },
+  "Hello world from react"
+);
+
+const heading2 = React.createElement(
+  "h1",
+  { id: "heading",key : 2 },
+  "Hello world from react2"
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [heading, heading2])
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
