e2396143ed7da642023f8107520d69b5d820654e
Zohaib ur Rehman <mianzhb@gmail.com>
2026-02-17 23:23:53 +0500

    after so long

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,17 +5,36 @@ import { useState } from "react";
 
 const Body = () => {
  const [list,setList] = useState(RESTURANT_LIST);
+ const [isFilterApplied, setIsFilterApplied] = useState(false);
+ const [searchTerm, setSerachTerm] = useState('');
+
+ const filterTopRated = ()=>{
+  const filterd = list.filter((res)=>res.rating >= 4.2)
+  setList(filterd);
+  setIsFilterApplied(true)
+ }
+
+ const filterSearchTerm = () =>{
+    const filterd = list.filter((res)=> res.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setList(filterd);
+    setIsFilterApplied(true)
+ }
+
+ const removeFilter = ()=>{
+  setList(RESTURANT_LIST);
+  setIsFilterApplied(false)
+ }
 
   return (
     <div className="body">
-      {/* <div className="search">Search</div> */}
       <div className="filters">
-        <button className="filter-btn" onClick={()=>{
-            console.log('button clicked');
-            const filterdRes = list.filter((res)=>res.rating > 4.2);
-            setList(filterdRes)
-        }}
+        <div className="search-container">
+          <input type="search" value={searchTerm} onChange={(e)=>setSerachTerm(e.target.value)} />
+          <button onClick={filterSearchTerm}>search</button>
+        </div>
+        <button className="filter-btn" onClick={filterTopRated}
         >Top Rated Resturntans</button>
+        {isFilterApplied && <button onClick={removeFilter}>clear</button>}
       </div>
       <div className="resturant-container">
         {list.map((res, index) => (
